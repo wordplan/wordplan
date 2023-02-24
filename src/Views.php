02005572ef20e8c1e0affbb132b59c15424d9c05
Oleg <109225168+frescoref@users.noreply.github.com>
2023-02-25 01:23:23 +0300

    Init

---
+++ src/Views.php
@@ -0,0 +1,13 @@
+<?php namespace Wordplan;
+
+defined('ABSPATH') || exit;
+
+/**
+ * Views
+ *
+ * @package Wordplan
+ */
+final class Views extends \Digiom\Woplucore\Views
+{
+    protected $slug = 'wordplan';
+}
