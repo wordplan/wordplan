e761c52c80f62daf92d6c992a17799498a10ce0f
Oleg <109225168+frescoref@users.noreply.github.com>
2023-03-11 10:55:33 +0300

    Fix: more

--- wordplan.php
+++ wordplan.php
@@ -29,7 +29,15 @@ namespace
 	{
 		define('WORDPLAN_PLUGIN_FILE', __FILE__);
 
-		include_once __DIR__ . '/vendor/autoload.php';
+		$autoloader = __DIR__ . '/vendor/autoload.php';
+
+		if(!is_readable($autoloader))
+		{
+			trigger_error('File is not found: ' . $autoloader);
+			return false;
+		}
+
+		require_once $autoloader;
 
 		/**
 		 * For external use
