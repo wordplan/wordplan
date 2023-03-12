aef9c5b7dd0b3f4e981787d45ea94e1318b964cd
Oleg <109225168+frescoref@users.noreply.github.com>
2023-03-12 13:08:04 +0300

    Init: more

---
+++ views/tools/empty.php
@@ -0,0 +1,6 @@
+<?php defined('ABSPATH') || exit; ?>
+
+<div class="wordplan-tools-alert mb-2 mt-2">
+    <h3><?php _e('Tools not found.', 'wordplan'); ?></h3>
+    <p><?php _e('As soon as the tools are installed, they will appear in this section.', 'wordplan'); ?></p>
+</div>
