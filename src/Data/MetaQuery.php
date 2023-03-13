51f5ea32c90c9b2f95c9d7abfdec3d838627d1e9
Oleg <109225168+frescoref@users.noreply.github.com>
2023-03-13 16:54:50 +0300

    Init: more

---
+++ src/Data/MetaQuery.php
@@ -0,0 +1,12 @@
+<?php namespace Wordplan\Data;
+
+defined('ABSPATH') || exit;
+
+/**
+ * Class MetaQuery
+ *
+ * @package Wordplan\Data
+ */
+class MetaQuery extends \Digiom\Woplucore\Data\MetaQuery
+{
+}
